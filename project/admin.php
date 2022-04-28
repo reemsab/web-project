e174c3812fe7663f1d8e4938fc65f89997e99052
reeemaa <58232514+reeemaa@users.noreply.github.com>
2022-04-28 07:20:28 +0300

    Update admin.php

--- project/admin.php
+++ project/admin.php
@@ -2,7 +2,30 @@
  <html>
    <head>
     <title>Admin</title>
+<script>
 
+function logInAdminButtonFunc(){
+    var flagLoginAdmin=true;
+    var textULoginAdmin=document.querySelector('#username').value;
+    var textPLoginAdmin=document.querySelector('#password').value;
+    if(textULoginAdmin==''){
+        document.getElementById('usernameHintLoginAdmin').innerHTML = "please enter your username";
+        flagLoginAdmin=false;
+
+    }else{
+        document.getElementById('usernameHintLoginAdmin').innerHTML = '';
+    }
+    if(textPLoginAdmin==''){
+        document.getElementById('passwordHintLoginAdmin').innerHTML = "please enter your password";
+        flagLoginAdmin=false;
+    }else{
+        document.getElementById('passwordHintLoginAdmin').innerHTML = '';
+    }
+
+return flagLoginAdmin;
+
+}
+</script>
 <?php
 
 
@@ -50,9 +73,11 @@ else
    
    <body>
   
-      <form name="form" action="" method="post">
+      <form name="form" onsubmit="return logInAdminButtonFunc()" action="" method="post">
         <input type="text" id="username" name= "username" placeholder="Enter your username"/>
+        <span id="usernameHintLoginAdmin" style="color:red;"></span>
         <input type="Password"  id="password" name= "password" placeholder="Enter your password"/>
+        <span id="passwordHintLoginAdmin" style="color:red;"></span>
         <input type = "submit" value = " Submit "/>
       </form>
 
